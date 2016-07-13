590fa32fda00641f1af9609b2370cf2ef9818918
myjournal <getmyjournal@gmail.com>
2016-07-13 08:45:04 +0000

    truncating other tables when criteria changes

--- app/models/sectionDao.js
+++ app/models/sectionDao.js
@@ -1,9 +1,12 @@
 "use strict";
 
 var db = require('./index.js');
-
+var DISABLE_FOREIGN_KEY_CHECKS = "SET FOREIGN_KEY_CHECKS=0";
+var ENABLE_FOREIGN_KEY_CHECKS = "SET FOREIGN_KEY_CHECKS=1";
 var selectQuery = "select * from appraisal_score.criteria";
-var truncateQuery = "truncate table appraisal_score.criteria";
+var truncateCriteriaQuery = "truncate table appraisal_score.criteria";
+var truncateCriteriaScoreQuery = "truncate table appraisal_score.section_score;";
+var truncateFinalScoreQuery = "truncate table appraisal_score.overall_score";
 var insertQuery = "INSERT INTO appraisal_score.criteria(measureName,weightage) values ?";
 
 var sectionDao = {
@@ -14,14 +17,19 @@ var sectionDao = {
     });
     connection
       .then(function(conn) {
-        return conn.query(selectQuery)
-          .then(function(rows) {
-            return rows.length ?
-              conn.query(truncateQuery)
-              .then(function() {
-                return conn;
-              }) : conn;
-          });
+        return conn.query(truncateFinalScoreQuery) && conn;
+      })
+      .then(function(conn) {
+        return conn.query(truncateCriteriaScoreQuery) && conn;
+      })
+      .then(function(conn) {
+        return conn.query(DISABLE_FOREIGN_KEY_CHECKS) && conn;
+      })
+      .then(function(conn) {
+        return conn.query(truncateCriteriaQuery) && conn;
+      })
+      .then(function(conn) {
+        return conn.query(ENABLE_FOREIGN_KEY_CHECKS) && conn;
       })
       .then(function(conn) {
         return conn.query(insertQuery, [refinedSectionRecords])
